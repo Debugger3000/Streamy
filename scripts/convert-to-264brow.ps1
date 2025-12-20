18a140eeb51844ea084765ec01e257ed572838ca
Debugger3000 <145146370+Debugger3000@users.noreply.github.com>
2025-12-19 21:04:54 -0500

    UX for mobile + more

--- scripts/convert-to-264brow.ps1
+++ scripts/convert-to-264brow.ps1
@@ -2,6 +2,14 @@
 # Convert english .mkvs to x264, 8 bit, aac encoding for normal browser viewing... still maintains resolution
 
 
+# Set your global markers (used for ALL files)
+$startMarker = Read-Host "Enter the START marker - Inclusive (e.g., S03)"
+$endMarker   = Read-Host "Enter the END marker - Non-inclusive(e.g., (10)"
+
+# Regex: match everything from START up to (but not including) END
+$regex = [regex]::Escape($startMarker) + "(.*?)(?=" + [regex]::Escape($endMarker) + ")"
+
+
 # Ask for output directory
 $outputDir = Read-Host "Enter the output directory (e.g., X:\streamy\media\shows\)"
 if (!(Test-Path $outputDir)) {
@@ -13,7 +21,7 @@ $files = Get-ChildItem -Path . -Filter *.mkv -File
 
 foreach ($file in $files) {
     # Match pattern like S01E04 (one letter, two digits, one letter, two digits)
-    if ($file.Name -match "[A-Z]\d{2}[A-Z]\d{2}") {
+    if ($file.Name -match $regex) {
         $episodeCode = $matches[0]
 
         # Set output filename
